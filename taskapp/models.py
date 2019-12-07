b7728e9c3cb9dadfad3393ae5ad00fc95789ff60
miguelfajardoc <775@holbertonschool.com>
2019-12-07 22:40:33 +0000

    [create] first basic api

---
+++ taskapp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class User(models.Model):
+    name = models.CharField(max_length=60, default='')
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    description = models.CharField(max_length=200, default='')
+    STATE_CHOICES = [
+        ('TD', 'to do'),
+        ('DN', 'done'),
+    ]
+    state = models.CharField(max_length=2, choices = STATE_CHOICES, default = 'TD')
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
